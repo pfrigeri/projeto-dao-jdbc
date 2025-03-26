2d5f0d6df5b7b8f0c6677824fa1b8a039ce79d78
pfrigeri <asockmf@gmail.com>
2025-03-26 08:55:25 -0300

    Departmente entity class

---
+++ src/application/program.java
@@ -0,0 +1,10 @@
+package application;
+
+import model.entities.Department;
+
+public class program {
+    public static void main(String[] args) {
+        Department d = new Department(12, "Books");
+        System.out.println(d);
+    }
+}
---
+++ src/model/entities/Department.java
@@ -0,0 +1,56 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer ID;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Integer ID, String name) {
+        this.ID = ID;
+        this.name = name;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Department that)) return false;
+        return Objects.equals(ID, that.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(ID);
+    }
+
+    @Override
+    public String toString() {
+        return  "ID : " + ID +
+                ", name: " + name;
+    }
+}
